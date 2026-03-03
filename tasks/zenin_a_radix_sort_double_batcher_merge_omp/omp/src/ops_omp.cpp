9ff7d80706ddbc4517f164d5566e1be676a7dc7e
AntonZenin <zenin.anton.2005@gmail.com>
2026-03-03 17:43:44 +0300

    initial commit

---
+++ tasks/zenin_a_radix_sort_double_batcher_merge_omp/omp/src/ops_omp.cpp
@@ -0,0 +1,37 @@
+#include "zenin_a_radix_sort_double_batcher_merge_omp/omp/include/ops_omp.hpp"
+#include <atomic>
+#include <numeric>
+#include <vector>
+
+#include "zenin_a_radix_sort_double_batcher_merge_omp/common/include/common.hpp"
+#include "util/include/util.hpp"
+
+namespace zenin_a_radix_sort_double_batcher_merge_omp {
+
+ZeninARadixSortDoubleBatcherMergeOMP::ZeninARadixSortDoubleBatcherMergeOMP(const InType &in) {
+  SetTypeOfTask(GetStaticTypeOfTask());
+  GetInput() = in;
+  GetOutput() = {};
+}
+
+bool ZeninARadixSortDoubleBatcherMergeOMP::ValidationImpl() {
+  return true;
+}
+
+bool ZeninARadixSortDoubleBatcherMergeOMP::PreProcessingImpl() {
+  return true;
+}
+
+bool ZeninARadixSortDoubleBatcherMergeOMP::RunImpl() {
+//#pragma omp parallel default(none) shared(counter) num_threads(ppc::util::GetNumThreads())
+  
+
+  
+  return true;
+}
+
+bool ZeninARadixSortDoubleBatcherMergeOMP::PostProcessingImpl() {
+  return true;
+}
+
+}  // namespace zenin_a_radix_sort_double_batcher_merge_omp
