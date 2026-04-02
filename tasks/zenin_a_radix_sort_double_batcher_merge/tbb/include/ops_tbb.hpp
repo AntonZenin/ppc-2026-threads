e6baff8209fce7d51af5cf753d9b439f75a876ab
AntonZenin <zenin.anton.2005@gmail.com>
2026-04-02 17:11:02 +0300

    started lab tbb

---
+++ tasks/zenin_a_radix_sort_double_batcher_merge/tbb/include/ops_tbb.hpp
@@ -0,0 +1,28 @@
+#pragma once
+
+#include "zenin_a_radix_sort_double_batcher_merge/common/include/common.hpp"
+#include "task/include/task.hpp"
+
+namespace zenin_a_radix_sort_double_batcher_merge {
+
+class ZeninARadixSortDoubleBatcherMergeTBB : public BaseTask {
+ public:
+  static constexpr ppc::task::TypeOfTask GetStaticTypeOfTask() {
+    return ppc::task::TypeOfTask::kTBB;
+  }
+  explicit ZeninARadixSortDoubleBatcherMergeTBB(const InType &in);
+
+ private:
+  bool ValidationImpl() override;
+  bool PreProcessingImpl() override;
+  bool RunImpl() override;
+  bool PostProcessingImpl() override;
+
+  static void BlocksComparing(std::vector<double> &arr, size_t i, size_t j);
+  static uint64_t PackDouble(double v) noexcept;
+  static double UnpackDouble(uint64_t k) noexcept;
+  static void LSDRadixSort(std::vector<double> &arr);
+  static void BatcherOddEvenMerge(std::vector<double> &arr, size_t n);
+};
+
+}  // namespace zenin_a_radix_sort_double_batcher_merge
